a93738ff6e655a8a4c98858eb438d4fb678911b5
okewunmi <okewunmiafeezolaide@gmail.com>
2022-05-15 04:09:54 +0100

    state

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@
 /** initialization */
 let count = 0;
 
-
 const btn = document.querySelectorAll('.btn');
 const value = document.querySelector('.value');
 
@@ -32,28 +31,32 @@ value.textContent = count;
 /*end of button counter */
 
 /** background nav button  */
- const btnNav = document.querySelectorAll('.button__nav');
- const header = document.querySelector('.header');
- btnNav.forEach(function(btnNav){
-  btnNav.addEventListener('click', function(e){
-   const style = e.currentTarget.classList;
-   if (style.contains('btn--nav-1')){
-    // document.querySelector('.header').style.backgroundImage = "url('background2.jpeg')";
-    header.classList.add("backgrd--1");
-   }
-   else if (style.contains('btn--nav-2')){
-    header.classList.add("backgrd--3");
+ // const btnNav = document.querySelectorAll('.button__nav');
+ // const header = document.querySelector('.header');
+ // btnNav.forEach(function(btnNav){
+ //  btnNav.addEventListener('click', function(e){
+ //   const style = e.currentTarget.classList;
+ //   if (style.contains('btn--nav-1')){
+ //    document.querySelector('.header').style.backgroundImage = "url('background2.jpeg')";
+    
+ //    // header.classList.add("backgrd--1");
+ //   }
+ //   else if (style.contains('btn--nav-2')){
+ //    header.classList.add("backgrd--3");
+ //    // document.querySelector('.header').style.backgroundImage = "url(initial)";
+ //   }
+ //   else if (style.contains('btn--nav-3')){
+ //    header.classList.add("backgrd--4");
+ //   }
+ //   else {
+ //    document.querySelector('.header').style.backgroundImage = "url(initial)";
 
-   }
-   else if (style.contains('btn--nav-3')){
-    header.classList.add("backgrd--4");
+ //   }
 
-   }
-   
-   else {
-    header.classList.add("backgrd--4");
+ //  })
+ // });
 
-   }
 
-  })
- });
+ /**start of the  testimony nav*/
+ 
+
